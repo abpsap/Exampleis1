fef5454753a9d07865ee8798e91d89bdd2f3d71c
Anand Patil <abpsap@gmail.com>
2024-05-18 19:47:57 -0400

    this is example proj

---
+++ exampl1.py
@@ -0,0 +1,11 @@
+from hashlib import sha256
+
+print ("hello")
+fav_lang = 'python   '
+fav_lang = fav_lang.rstrip()
+print (f"The output is {fav_lang}")
+
+msg = b'asjkhdkajsdh'
+hash_function = sha256(msg)
+print (hash_function.digest())
+
