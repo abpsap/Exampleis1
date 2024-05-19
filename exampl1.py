e6c99f6c057871f0181a0d2d8512a7cc734bf482
Anand Patil <abpsap@gmail.com>
2024-05-19 12:48:41 -0400

    ok

--- exampl1.py
+++ exampl1.py
@@ -1,3 +1,4 @@
+import string
 from hashlib import sha256
 
 print ("hello")
@@ -9,6 +10,38 @@ msg = b'asjkhdkajsdh'
 hash_function = sha256(msg)
 print (hash_function.digest())
 
-bro = ["jack", "jill", "mohan", "guru"]
-brostr = ', '.join(bro)
+bro = ["^$%jack;&%$", "jill*&^", "mohan))#$", "guru$^*&"]
+brostr = '; '.join(bro)
 print (f"the joined brostr is {brostr}")
+#brostrstrip = brostr.replace(string.punctuation, '6')
+brostrstrip = brostr.strip(string.punctuation)
+print (f"{brostrstrip}")
+
+crypto_list = ['Yeti', 'Bigfoot', 'Loch Ness Monster']
+crypto_string = ', '.join(crypto_list)
+
+print (f"{crypto_string}")
+vowels = ( 'a' \
+    "e" '''i''' \
+    'o' """u""" \
+    )
+print (vowels)
+
+# for x in range(12, 1, -1):
+#    print(x)
+boxc = 'returntheitem'
+print (boxc[::-1])
+
+marxes = ['Groucho', 'Chico', 'Harpo']
+print (f"{marxes[0:3]}")
+marxes.insert(1, 'Zeppo')
+print (f"{marxes[0:4]}")
+
+horz = range (2,9)
+vert = range (1,11)
+
+mlist = [(row, col) for (row, col) in zip (horz, vert)]
+print (mlist)
+
+mlist = [(row, col) for (row, col) in zip (horz, vert) if row %2 != 1]
+print (mlist)
